e6e04054e6f465662026dadbc0f139a553f014d3
MobDe <MobDevX.outlook.com>
2021-03-29 19:16:28 +0700

    Back&Next

--- app/src/main/java/a/navdemo/MainActivity.java
+++ app/src/main/java/a/navdemo/MainActivity.java
@@ -1,19 +1,40 @@
 package a.navdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
-        }
+
+        navController = Navigation.findNavController(this, R.id.nav_host);
     }
+
+    public void onFragment1NextClick() {
+        navController.navigate(R.id.fragment2);
+    }
+
+    public void onFragment1BackClick() {}
+
+    public void onFragment2NextClick() {
+        navController.navigate(R.id.fragment3);
+    }
+
+    public void onFragment2BackClick() {
+        navController.popBackStack();
+    }
+
+    public void onFragment3NextClick() {}
+
+    public void onFragment3BackClick() {
+        navController.popBackStack();
+    }
+
 }
