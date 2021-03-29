585bc6fe8d4770bfc297be5689e3c81b1e272815
MobDe <MobDevX.outlook.com>
2021-03-29 20:44:06 +0700

    Add Fragments

--- app/src/main/java/a/navdemo/MainActivity.java
+++ app/src/main/java/a/navdemo/MainActivity.java
@@ -10,10 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
+
+
+/*        if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
-        }
+        }*/
     }
 }
